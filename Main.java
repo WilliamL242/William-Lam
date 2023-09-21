5201b87bfa8226947ea9bc98eb8df789f18a4fdc
WilliamL242 <145403978+WilliamL242@users.noreply.github.com>
2023-09-20 21:09:59 -0400

    Main.java L1.2

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
 public class Main
 {
-    public class void main(String[] args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello world!");
-        Syste.out.print("Hello world!");
+// final makes the variable a constant
+        final Integer i = 7;
+        // integer stores integers
+        Boolean bool = false;
+        // boolean stores a value that is either true or false
+        Double d = 3.14;
+        // double stores floats
+
+        System.out.println(i);
+        System.out.println(bool);
+        System.out.println(d);
+
     }
 }
