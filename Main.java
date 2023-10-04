72cfe9a1c3363970c3197daca70538dd906ac6d6
WilliamL242 <145403978+WilliamL242@users.noreply.github.com>
2023-10-03 21:50:31 -0400

    2.4

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
         // final makes the variable a constant
@@ -16,14 +14,13 @@ public class Main
 
 
         Integer a = (7 + 27) * 3;
-        System.out.println((double)a);
+        System.out.println((double) a);
         Integer IntOverFlow = Integer.MAX_VALUE + a;
 
         System.out.println(IntOverFlow);
         System.out.println(i);
         System.out.println(b);
         System.out.println(d);
-
 
 
         Pie Blueberry_Pie = new Pie(true, "blueberry");
@@ -33,10 +30,14 @@ public class Main
 
         System.out.println("Enter a type of pie:");
         String pie_type = s.nextLine();
+        System.out.println("Enter number of slices you want to eat: ");
+        int slices_eaten = s.nextInt();
 
         Pie User_pie = new Pie(true, true, pie_type);
         String pie_insides = User_pie.getFilling();
         String pie_crust = User_pie.getCrust();
         System.out.println(pie_insides);
         System.out.println(pie_crust);
-        User_pie.eat();
+        User_pie.eat(slices_eaten);
+    }
+}
