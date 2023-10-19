6c952ec7db3b26f5eaa7a2b03dc4fda2f164cdbe
WilliamL242 <145403978+WilliamL242@users.noreply.github.com>
2023-10-19 14:02:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,10 +7,9 @@ public class Main {
         // final makes the variable a constant
         final Integer c = 100;
         // integer stores integers
-        Boolean b = false;
-        // boolean stores a value that is either true or false
         Double d = 3.14;
         // double stores floats
+        String best_pie = "Keylime";
 
 
         Integer a = (7 + 27) * 3;
@@ -18,7 +17,6 @@ public class Main {
         Integer IntOverFlow = Integer.MAX_VALUE + a;
 
         System.out.println(IntOverFlow);
-        System.out.println(b);
         System.out.println(d);
 
 
@@ -31,11 +29,19 @@ public class Main {
         String pie_type = s.nextLine();
         System.out.println("Enter number of slices you want to eat: ");
         int slices_eaten = s.nextInt();
+        Boolean is_best_pie = pie_type.equals(best_pie);
+        if (is_best_pie == true){
+            System.out.println("Good choice");
+        }
+        else{
+            System.out.println("Not the best but all pies are good");
+        }
 
         Pie User_pie = new Pie(true, true, pie_type);
         String pie_insides = User_pie.getFilling();
         String pie_crust = User_pie.getCrust();
         User_pie.eat(slices_eaten);
         User_pie.calories(c, slices_eaten);
+        System.out.println(User_pie);
     }
 }
