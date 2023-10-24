acdfa6b9ac1421bcb0331c70ab22f136da80c039
WilliamL242 <145403978+WilliamL242@users.noreply.github.com>
2023-10-24 14:02:42 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,31 +5,17 @@ public class Main {
         Scanner s = new Scanner(System.in);
 
         // final makes the variable a constant
-        final Integer c = 100;
+        final Integer c = 350; //calories per slice of pie
         // integer stores integers
         Double d = 3.14;
         // double stores floats
-        String best_pie = "Keylime";
-
-
-        Integer a = (7 + 27) * 3;
-        System.out.println((double) a);
-        Integer IntOverFlow = Integer.MAX_VALUE + a;
-
-        System.out.println(IntOverFlow);
-        System.out.println(d);
-
-
-        Pie Blueberry_Pie = new Pie(true, "blueberry");
-        String filling = Blueberry_Pie.getFilling();
-        System.out.println(filling);
-        Blueberry_Pie.eat();
+        String best_pie = "keylime";
 
         System.out.println("Enter a type of pie:");
         String pie_type = s.nextLine();
         System.out.println("Enter number of slices you want to eat: ");
         int slices_eaten = s.nextInt();
-        Boolean is_best_pie = pie_type.equals(best_pie);
+        Boolean is_best_pie = pie_type.toLowerCase().equals(best_pie);
         if (is_best_pie == true){
             System.out.println("Good choice");
         }
