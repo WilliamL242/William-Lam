77b9b3d13dceaa97fc9ac77f77100fb55e185e10
WilliamL242 <145403978+WilliamL242@users.noreply.github.com>
2023-11-01 14:06:27 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
+        String[][] map = {{"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
+                {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
+                {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
+                {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
+                {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
+                {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
+                {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
+                {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"}};
         // final makes the variable a constant
         final Integer c = 350; //calories per slice of pie
         // integer stores integers
