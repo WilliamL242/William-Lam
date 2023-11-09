83a2ad1e709d5ee5b692204bcd475fa2da3b027b
WilliamL242 <145403978+WilliamL242@users.noreply.github.com>
2023-11-09 14:05:11 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String[][] map = {{"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
+        String[][] map = {{"\uD83D\uDDFF", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
                 {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
                 {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
                 {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
@@ -11,30 +11,35 @@ public class Main {
                 {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
                 {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
                 {"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"}};
-        // final makes the variable a constant
+
+        Map layout = new Map();
+
+        layout.print_map(map);
+
+
         final Integer c = 350; //calories per slice of pie
-        // integer stores integers
-        Double d = 3.14;
-        // double stores floats
-        String best_pie = "keylime";
-
-        System.out.println("Enter a type of pie:");
-        String pie_type = s.nextLine();
-        System.out.println("Enter number of slices you want to eat: ");
-        int slices_eaten = s.nextInt();
-        Boolean is_best_pie = pie_type.toLowerCase().equals(best_pie);
-        if (is_best_pie == true){
-            System.out.println("Good choice");
-        }
-        else{
-            System.out.println("Not the best but all pies are good");
-        }
-
-        Pie User_pie = new Pie(true, true, pie_type);
-        String pie_insides = User_pie.getFilling();
-        String pie_crust = User_pie.getCrust();
-        User_pie.eat(slices_eaten);
-        User_pie.calories(c, slices_eaten);
-        System.out.println(User_pie);
+//        // integer stores integers
+//        Double d = 3.14;
+//        // double stores floats
+//        String best_pie = "keylime";
+//
+//        System.out.println("Enter a type of pie:");
+//        String pie_type = s.nextLine();
+//        System.out.println("Enter number of slices you want to eat: ");
+//        int slices_eaten = s.nextInt();
+//        Boolean is_best_pie = pie_type.toLowerCase().equals(best_pie);
+//        if (is_best_pie == true){
+//            System.out.println("Good choice");
+//        }
+//        else{
+//            System.out.println("Not the best but all pies are good");
+//        }
+//
+//        Pie User_pie = new Pie(true, true, pie_type);
+//        String pie_insides = User_pie.getFilling();
+//        String pie_crust = User_pie.getCrust();
+//        User_pie.eat(slices_eaten);
+//        User_pie.calories(c, slices_eaten);
+//        System.out.println(User_pie);
     }
 }
